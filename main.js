4a4c25dbfc68f952a95fc943abe8386655d4949a
ertegün fidan <ertegunfidan@gmail.com>
2017-03-16 17:25:46 +0300

    tray hatası düzeltildi

--- main.js
+++ main.js
@@ -26,86 +26,31 @@ function createWindow() {
 app.on('ready', () => {
   createWindow();
 
-
-  win.onbeforeunload = (e) => {
-    console.log('I do not want to be closed')
-
-    // Unlike usual browsers that a message box will be prompted to users, returning
-    // a non-void value will silently cancel the close.
-    // It is recommended to use the dialog API to let the user confirm closing the
-    // application.
-    e.returnValue = false
-  }
-
-  win.on('close', (e) => {
-    console.log("close");
-//    console.log(e);
-
-    createWindow();
-//  event.preventDefault();
-    win.hide();
-
-    tray = new Tray('app/img/logo.ico')
-    const contextMenu = Menu.buildFromTemplate([
-      {
-        label: 'Aç',
-        accelerator: 'Alt+I',
-        click: function () {
-          win.show();
-//        win.toggleDevTools();
-        }
-      },
-      {label: 'Çıkış',
-        click: function () {
-          win = null
-          app.quit()
-        }
+  tray = new Tray('app/img/logo.ico')
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Aç',
+      accelerator: 'Alt+I',
+      click: function () {
+        win.show();
       }
-    ]);
-    tray.setToolTip('Kritik Yedekleme')
-    tray.setContextMenu(contextMenu)
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-//    win = null
-  })
-  win.on('closed', (e) => {
-    console.log("closed");
-//    console.log(e);
-    e.returnValue = false
-  })
+    },
+    {label: 'Çıkış',
+      click: function () {
+        win = null
+        app.quit()
+      }
+    }
+  ]);
+  tray.setToolTip('Kritik Yedekleme')
+  tray.setContextMenu(contextMenu)
 
-  win.onbeforeunload = (e) => {
-    console.log('I do not want to be closed')
-
-    // Unlike usual browsers that a message box will be prompted to users, returning
-    // a non-void value will silently cancel the close.
-    // It is recommended to use the dialog API to let the user confirm closing the
-    // application.
-    e.returnValue = false
-  }
-  win.unload  = (e) => {
-  console.log('I do not want to be closed')
-
-  // Unlike usual browsers that a message box will be prompted to users, returning
-  // a non-void value will silently cancel the close.
-  // It is recommended to use the dialog API to let the user confirm closing the
-  // application.
-  e.returnValue = false
-}
-
+  win.on('close', (event) => {
+    console.log("close");
+    if (!app.isQuiting) {
+      event.preventDefault()
+      win.hide();
+    }
+    return false;
+  });
 })
-
-app.on('window-all-closed', () => {
-//  if (process.platform !== 'darwin') {
-//    app.quit()
-//  win.hide();
-//  }
-})
-
-app.on('activate', () => {
-  if (win == null) {
-//    createWindow()
-  }
-})
-
